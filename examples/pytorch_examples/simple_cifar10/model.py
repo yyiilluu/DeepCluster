d191fbc9e9363aa8a8c95486aea6da429686206a
Yi Lu <yilu.louis@outlook.com>
2019-04-09 23:30:13 -0700

    update example with command

--- examples/pytorch_examples/simple_cifar10/model.py
+++ examples/pytorch_examples/simple_cifar10/model.py
@@ -100,34 +100,3 @@ def train_model(dataset_dir, output_dir, config):
   _, predicted = torch.max(outputs, 1)
 
   print('Predicted: ', ' '.join('%5s' % classes[predicted[j]] \
-for j in range(4)))
-
-  correct = 0
-  total = 0
-  with torch.no_grad():
-      for data in testloader:
-          images, labels = data
-          outputs = net(images)
-          _, predicted = torch.max(outputs.data, 1)
-          total += labels.size(0)
-          correct += (predicted == labels).sum().item()
-
-  print('Accuracy of the network on the 10000 test images: %d %%' % (
-      100 * correct / total))
-
-  class_correct = list(0. for i in range(10))
-  class_total = list(0. for i in range(10))
-  with torch.no_grad():
-    for data in testloader:
-      images, labels = data
-      outputs = net(images)
-      _, predicted = torch.max(outputs, 1)
-      c = (predicted == labels).squeeze()
-      for i in range(4):
-        label = labels[i]
-        class_correct[label] += c[i].item()
-        class_total[label] += 1
-
-  for i in range(10):
-    print('Accuracy of %5s : %2d %%' % (classes[i], \
-      100 * class_correct[i] / class_total[i]))
